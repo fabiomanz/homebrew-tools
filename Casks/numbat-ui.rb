e6b2195b34460e70b3232af8dfd6987dbb3fe931
Fabio <fabiomanz@icloud.com>
2026-01-01 13:53:31 +0100

    Update SHA256 checksum for Numbat UI cask

--- Casks/numbat-ui.rb
+++ Casks/numbat-ui.rb
@@ -1,6 +1,6 @@
 cask "numbat-ui" do
   version "1.0.0"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "1b8cbf98d73e827d76e0707a60f7ed8d4e63099810221a29eba66106f71fcd71"
 
   url "https://github.com/fabiomanz/numbat-ui/releases/download/v#{version}/numbat-ui_#{version}_universal.dmg"
   name "Numbat UI"
