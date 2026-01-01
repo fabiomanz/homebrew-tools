a9aadf8fc4e1b934a3c6d4aef9d907b2f81085fb
Fabio <fabiomanz@icloud.com>
2026-01-01 13:50:30 +0100

    Fix URL for Numbat UI cask

--- Casks/numbat-ui.rb
+++ Casks/numbat-ui.rb
@@ -2,10 +2,10 @@ cask "numbat-ui" do
   version "1.0.0"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
-  url "https://github.com/fabiomanz/numbat_ui/releases/download/v#{version}/numbat-ui_#{version}_universal.dmg"
+  url "https://github.com/fabiomanz/numbat-ui/releases/download/v#{version}/numbat-ui_#{version}_universal.dmg"
   name "Numbat UI"
   desc "A native GUI for the Numbat scientific calculator"
-  homepage "https://github.com/fabiomanz/numbat_ui"
+  homepage "https://github.com/fabiomanz/numbat-ui"
 
   app "numbat-ui.app"
 
