48a4722f865b3d16c64ca8045423016dea31a7cc
Fabio Manz <fabio@aquilo-it.com>
2026-01-01 15:54:54 +0100

    fix app name

--- Casks/numbat-ui.rb
+++ Casks/numbat-ui.rb
@@ -2,12 +2,12 @@ cask "numbat-ui" do
   version "1.0.1"
   sha256 "8424caaed0a69d07ba63c46133a4ed85ced5189895c0b29aa2c4075ea94188df"
 
-  url "https://github.com/fabiomanz/numbat-ui/releases/download/v1.0.1/Numbat_1.0.1_universal.dmg"
+  url "https://github.com/fabiomanz/numbat-ui/releases/download/v#{version}/Numbat_#{version}_universal.dmg"
   name "Numbat UI"
   desc "A native GUI for the Numbat scientific calculator"
   homepage "https://github.com/fabiomanz/numbat-ui"
 
-  app "numbat-ui.app"
+  app "Numbat.app"
 
   zap trash: [
     "~/Library/Application Support/com.fabio.numbat-ui",
