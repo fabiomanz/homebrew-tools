50664af1479a211290e7c28ca1fbd20a096a41bf
Fabio <fabiomanz@icloud.com>
2026-01-02 21:20:37 +0100

    Update numbat-ui formula to 1.0.2

--- Casks/numbat-ui.rb
+++ Casks/numbat-ui.rb
@@ -1,8 +1,8 @@
 cask "numbat-ui" do
-  version "1.0.1"
-  sha256 "8424caaed0a69d07ba63c46133a4ed85ced5189895c0b29aa2c4075ea94188df"
+  version "1.0.2"
+  sha256 "261892ab773860fe94815db968448c6df89a1c4424207f6117208b41b08a9805"
 
-  url "https://github.com/fabiomanz/numbat-ui/releases/download/v#{version}/Numbat_#{version}_universal.dmg"
+  url "https://github.com/fabiomanz/numbat-ui/releases/download/v1.0.2/Numbat_1.0.2_universal.dmg"
   name "Numbat UI"
   desc "A native GUI for the Numbat scientific calculator"
   homepage "https://github.com/fabiomanz/numbat-ui"
