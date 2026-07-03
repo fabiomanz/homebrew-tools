7de8a16e8e72e4de9bb896efbf3cb46aa962016a
Fabio <fabiomanz@icloud.com>
2026-07-03 17:59:45 +0200

    Update numbat-ui formula to 2.0.2

--- Casks/numbat-ui.rb
+++ Casks/numbat-ui.rb
@@ -1,8 +1,8 @@
 cask "numbat-ui" do
-  version "2.0.1"
-  sha256 "b6121c6b57eb9441e13bd9e9e529d1c32b4d7452c1de225d748c634db0b8c68d"
+  version "2.0.2"
+  sha256 "7f60e23794bee3e7337cba2b8f873ff0865016ceab7a6b2d73b6036e4f461d90"
 
-  url "https://github.com/fabiomanz/numbat-ui/releases/download/v2.0.1/numbat-ui-macos-aarch64.zip"
+  url "https://github.com/fabiomanz/numbat-ui/releases/download/v2.0.2/numbat-ui-macos-universal.zip"
   name "Numbat UI"
   desc "A native GUI for the Numbat scientific calculator"
   homepage "https://github.com/fabiomanz/numbat-ui"
